91f853325bac8c0d36703a49ae73c6feb01cee0a
deadlockeenside <deadlockeenside@proton.me>
2025-10-16 18:11:25 +0300

    Add IStockItemRepository

--- AirSmileWMS.Domain/Interfaces/IStockItemRepository.cs
+++ AirSmileWMS.Domain/Interfaces/IStockItemRepository.cs
@@ -1,5 +1,8 @@
-﻿namespace AirSmileWMS.Domain.Interfaces;
+﻿using AirSmileWMS.Domain.Entities;
+
+namespace AirSmileWMS.Domain.Interfaces;
 
 public interface IStockItemRepository
 {
+    IReadOnlyList<StockItem> GetAllLatex();
 }
