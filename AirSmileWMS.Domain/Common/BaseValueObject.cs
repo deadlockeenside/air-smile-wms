7e0832a066c66c3454ca4a3e11b01c3d1332c771
deadlockeenside <deadlockeenside@proton.me>
2025-10-16 15:58:49 +0300

    Add base value object

---
+++ AirSmileWMS.Domain/Common/BaseValueObject.cs
@@ -0,0 +1,18 @@
+﻿namespace AirSmileWMS.Domain.Common;
+
+public abstract record BaseValueObject<T> where T : notnull
+{
+    public static implicit operator T(BaseValueObject<T> obj) => obj is null ? throw new ArgumentNullException(nameof(obj)) : obj.Value;
+
+    protected BaseValueObject(T value)
+    {
+        Validate(value);
+        Value = value;
+    }
+
+    public T Value { get; private init; }
+
+    protected abstract void Validate(T value);
+
+    public sealed override string ToString() => Value?.ToString() ?? string.Empty;
+}
