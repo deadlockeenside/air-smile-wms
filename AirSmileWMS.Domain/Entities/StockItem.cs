e9c206761b669c0d3d1fe1fdca457b8d9cc4f472
deadlockeenside <deadlockeenside@proton.me>
2025-10-16 16:27:41 +0300

    Update stock item

--- AirSmileWMS.Domain/Entities/StockItem.cs
+++ AirSmileWMS.Domain/Entities/StockItem.cs
@@ -5,8 +5,16 @@ namespace AirSmileWMS.Domain.Entities;
 
 public sealed class StockItem : BaseEntity
 {
+    private Title _title = null!;
+
     // TODO: Image
-    public required Title Title { get; set; }
+
+    public required Title Title 
+    { 
+        get => _title;
+        set => _title = value ?? throw new InvalidDataException(ExceptionMessages.EmptyTitle);
+    }
+
     // TODO: Pieces per package
     // TODO: Stock item comment
     // TODO: Monthly consumption
