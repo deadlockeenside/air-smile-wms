24321e6f644832d1e4b1c055b6152729f6a5bbf7
deadlockeenside <deadlockeenside@proton.me>
2025-10-16 15:47:34 +0300

    Update base entity

--- AirSmileWMS.Domain/Common/BaseEntity.cs
+++ AirSmileWMS.Domain/Common/BaseEntity.cs
@@ -1,8 +1,19 @@
 ﻿namespace AirSmileWMS.Domain.Common;
 
 // This can easily be modified to be BaseEntity<T> and public T Id to support different key types.
-public class BaseEntity
+public abstract class BaseEntity
 {
+    private int _id;
+
     // Using non-generic integer types for simplicity and to ease caching logic.
-    public required int Id { get; init; }
+    public required int Id 
+    {
+        get => _id;
+        init => _id = value != 0 ? value : throw new InvalidDataException(ExceptionMessages.EmptyId);
+    }
+
+    public sealed override bool Equals(object? obj) => obj is BaseEntity other && Id == other.Id && GetType() == other.GetType();
+    public sealed override int GetHashCode() => Id.GetHashCode();
+    public static bool operator ==(BaseEntity? a, BaseEntity? b) => Equals(a, b);
+    public static bool operator !=(BaseEntity? a, BaseEntity? b) => !Equals(a, b);
 }
