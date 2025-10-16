66a673aa40660a8b57dce7a615b75cc3bcebd324
deadlockeenside <deadlockeenside@proton.me>
2025-10-16 15:14:49 +0300

    Add empty stock item

---
+++ AirSmileWMS.Domain/Entities/StockItem.cs
@@ -0,0 +1,13 @@
+﻿namespace AirSmileWMS.Domain.Entities;
+
+public sealed class StockItem
+{
+    // TODO: Id
+    // TODO: Image
+    // TODO: Name
+    // TODO: Pieces per package
+    // TODO: Stock item comment
+    // TODO: Monthly consumption
+    // TODO: Stock balance
+    // TODO: Reorder quantity (calculated as: monthly consumption − stock balance)
+}
